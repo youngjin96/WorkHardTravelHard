be07c0133fefb917884d712224866a37690190d7
송영진 <song-youngjin@song-yeongjin-ui-MacBook-Pro.local>
2021-11-17 21:42:53 +0900

    Add textInput

--- App.js
+++ App.js
@@ -1,15 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import { theme } from './color';
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+  const onChangeText = (payload) => setText(payload);
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.header}>
-        <Text style={styles.btnText}>Work</Text>
-        <Text style={styles.btnText}>Travel</Text>
+        {/* 터치 이벤트가 발생할때 listen할 수 있다. Opacity가 있는 이유는 누를때 투명해지는 효과를 얻기 위함
+            pressable이 새로 나왔다. */}
+        <TouchableOpacity onPress={work}>
+          {/* style을 object로 사용하면서 확장하는 법 {{... }} */}
+          <Text style={{...styles.btnText, color: working ? "white" : theme.grey}}>Work</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={travel}>
+          <Text style={{...styles.btnText, color: !working ? "white" : theme.grey}}>Travel</Text>
+        </TouchableOpacity>
       </View>
+      <TextInput
+        onChangeText={onChangeText}
+        value={text}
+        placeholder={working ? "Add To Do" : "Where To Go"}
+        style={styles.input}
+      />
     </View>
   );
 }
@@ -17,11 +36,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "space-between",
-    backgroundColor: "#000",
+    backgroundColor: theme.bg,
     paddingHorizontal : 20,
   },
   header: {
+    justifyContent: "space-between",
     flexDirection: "row",
     marginTop: 100,
     
@@ -29,6 +48,14 @@ const styles = StyleSheet.create({
   btnText: {
     fontSize: 38,
     fontWeight: "600",
-    color = "white",
+    color: "white",
   },
+  input:{
+    backgroundColor: "white",
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    borderRadius: 30,
+    marginTop: 20,
+    fontSize: 18,
+  }
 });
